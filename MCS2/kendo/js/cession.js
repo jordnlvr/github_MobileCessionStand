3cf1691afad4322076aa8d22ed7f128d43124536
jordnlvr <kellydseale@msn.com>
2014-10-15 12:45:51 -0500

    Lots of Goodies

---
+++ MCS2/kendo/js/cession.js
@@ -0,0 +1,14 @@
+/* Seale Core js v2014
+*  Copyright 2014 SealeCorp. All rights reserved.
+*/
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function goBack() {
+    app.navigate("#:back");
+}
