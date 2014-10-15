ffc9f1a1c451cc5f6e4e3d1a0359bf5f4872babf
jordnlvr <kellydseale@msn.com>
2014-10-15 15:11:47 -0500

    changes

--- MCS2/scripts/app.js
+++ MCS2/scripts/app.js
@@ -18,41 +18,46 @@ angular.module('todo', ['ionic'])
 
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        home: {
-          title: 'Home'
-        },
-        settings: {
-          title: 'Settings'
-        },
-        contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+        models: {
+            home: {
+                title: 'Home'
+            },
+            settings: {
+                title: 'Settings'
+            },
+            contacts: {
+                title: 'Contacts',
+                ds: new kendo.data.DataSource({
+                    data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
+                }),
+                alert: function (e) {
+                    alert(e.data.name);
+                }
+            }
         }
-      }
     };
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      //navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        //skin: 'flat',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        //navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        var analytics = window.plugins.EqatecAnalytics;
+        var settings = analytics.Factory.CreateSettings("91c6fa50595b44a7925bd96151f1add5", "1.06");
+        analytics.Factory.CreateMonitorWithSettings(settings, monitorCreated, monitorCreationFailed);
+
+
+        app = new kendo.mobile.Application(document.body, {
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            //skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/home.html'
+        });
 
     }, false);
 
